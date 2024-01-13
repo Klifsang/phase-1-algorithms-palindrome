39479744cc1157343a23aeb6caba9d9c945ee385
Klif <sang.wicklife@student.moringaschool.com>
2024-01-13 18:47:17 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let originWord = word.trim().toLowerCase();
+  let reversedWord = originWord.split('').reverse().join('');
+  return originWord==reversedWord;
+
 }
 
 /* 
   Add your pseudocode here
+  - Trim the word to remove leading and ending spaces if any.
+  - Change the word to lower case letters
+  - Create a reversed version of the word.
+  - Compare the original word to reversed word.
 */
+
 
 /*
   Add written explanation of your solution here
+  Split is use to change the word into an array of letters, which is then reversed and 
+  concatenated into a new word using join('')
+
 */
 
 // You can run `node index.js` to view these console logs
